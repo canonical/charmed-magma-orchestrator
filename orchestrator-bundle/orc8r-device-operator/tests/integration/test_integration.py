3d1699dce892e1a2959897d7db974a4150967579
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2021-11-23 18:20:38 +0100

    Fix: Fixing integration tests

--- orchestrator-bundle/orc8r-device-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-device-operator/tests/integration/test_integration.py
@@ -13,13 +13,14 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
 APPLICATION_NAME = "orc8r-device"
+CHARM_NAME = "magma-orc8r-device"
 
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     charm = await ops_test.build_charm(".")
     resources = {
-        f"{APPLICATION_NAME}-image": METADATA["resources"][f"{APPLICATION_NAME}-image"][
+        f"{CHARM_NAME}-image": METADATA["resources"][f"{CHARM_NAME}-image"][
             "upstream-source"
         ],
     }
