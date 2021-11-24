51cc3f1723717a3d3b18354d1d013136492ef158
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2021-11-24 12:33:06 +0100

    Feat: Adding separate workflow for each orc8r charm; updating tox.inis

--- orchestrator-bundle/orc8r-device-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-device-operator/tests/integration/test_integration.py
@@ -12,22 +12,28 @@ from pytest_operator.plugin import OpsTest  # type: ignore
 logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
-APPLICATION_NAME = "magma-orc8r-device"
+APPLICATION_NAME = "orc8r-device"
+CHARM_NAME = "magma-orc8r-device"
 
 
-@pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    charm = await ops_test.build_charm(".")
-    resources = {
-        f"{APPLICATION_NAME}-image": METADATA["resources"][f"{APPLICATION_NAME}-image"][
-            "upstream-source"
-        ],
-    }
-    await ops_test.model.deploy("postgresql-k8s", application_name="postgresql-k8s")
-    await ops_test.model.deploy(
-        charm, resources=resources, application_name=APPLICATION_NAME, trust=True
-    )
-    await ops_test.model.add_relation(relation1=APPLICATION_NAME, relation2="postgresql-k8s:db")
-    await ops_test.model.wait_for_idle(
-        apps=["postgresql-k8s", APPLICATION_NAME], status="active", timeout=1000
-    )
+class TestOrc8rDevice:
+    @pytest.fixture(scope="module")
+    async def setup(self, ops_test):
+        await ops_test.model.deploy("postgresql-k8s", application_name="postgresql-k8s")
+        await ops_test.model.wait_for_idle(apps=["postgresql-k8s"], status="active", timeout=1000)
+
+    @pytest.mark.abort_on_fail
+    async def test_build_and_deploy(self, ops_test, setup):
+        charm = await ops_test.build_charm(".")
+        resources = {
+            f"{CHARM_NAME}-image": METADATA["resources"][f"{CHARM_NAME}-image"]["upstream-source"],
+        }
+        await ops_test.model.deploy(
+            charm, resources=resources, application_name=APPLICATION_NAME, trust=True
+        )
+        await ops_test.model.add_relation(
+            relation1=APPLICATION_NAME, relation2="postgresql-k8s:db"
+        )
+        await ops_test.model.wait_for_idle(
+            apps=[APPLICATION_NAME], status="active", timeout=1000
+        )
