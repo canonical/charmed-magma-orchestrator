4b4067d38d6f06310512a8e8a8040d115872ed8f
guillaume <guillaume.belanger27@gmail.com>
2022-01-28 15:42:31 -0500

    - New charms use orc8r_base library (orc8r-obsidian) - New charms use orc8r_base_db library (orc8r-policydb, orc8r-smsd, orc8r-state, orc8r-subscriberdb, orc8r-subscriberdb-cache, orc8r-tenants)

--- orchestrator-bundle/orc8r-obsidian-operator/src/charm.py
+++ orchestrator-bundle/orc8r-obsidian-operator/src/charm.py
@@ -2,88 +2,29 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import logging
-
+from charms.magma_orc8r_libs.v0.orc8r_base_db import Orc8rBase
 from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
-from ops.pebble import ConnectionError, Layer
-
-logger = logging.getLogger(__name__)
 
 
 class MagmaOrc8rObsidianCharm(CharmBase):
-    """Charm the service."""
-
     def __init__(self, *args):
-        """
-        An instance of this object everytime an event occurs
-        """
+        """Creates a new instance of this object for each event."""
         super().__init__(*args)
-        self._container_name = self._service_name = "magma-orc8r-obsidian"
-        self._container = self.unit.get_container(self._container_name)
-        self.framework.observe(
-            self.on.magma_orc8r_obsidian_pebble_ready, self._on_magma_orc8r_obsidian_pebble_ready
-        )
         self._service_patcher = KubernetesServicePatch(
             charm=self,
             ports=[("grpc", 9180, 9093), ("http", 8080, 9081)],
             additional_labels={"app.kubernetes.io/part-of": "orc8r-app"},
         )
-
-    def _on_magma_orc8r_obsidian_pebble_ready(self, event):
-        """
-        Triggered when pebble is ready
-        """
-        self._configure_orc8r(event)
-
-    def _pebble_layer(self) -> Layer:
-        """Returns pebble layer for the charm."""
-        return Layer(
-            {
-                "summary": f"{self._service_name} layer",
-                "description": f"pebble config layer for {self._service_name}",
-                "services": {
-                    self._service_name: {
-                        "override": "replace",
-                        "summary": self._service_name,
-                        "startup": "enabled",
-                        "command": "/usr/bin/envdir "
-                        "/var/opt/magma/envdir "
-                        "/var/opt/magma/bin/obsidian "
-                        "-logtostderr=true -v=0",
-                        "environment": {
-                            "SERVICE_HOSTNAME": self._container_name,
-                            "SERVICE_REGISTRY_MODE": "k8s",
-                            "SERVICE_REGISTRY_NAMESPACE": self._namespace,
-                        },
-                    }
-                },
-            }
+        startup_command = (
+            "/usr/bin/envdir "
+            "/var/opt/magma/envdir "
+            "/var/opt/magma/bin/obsidian "
+            "-logtostderr=true "
+            "-v=0"
         )
-
-    def _configure_orc8r(self, event):
-        """
-        Adds layer to pebble config if the proposed config is different from the current one
-        """
-        pebble_layer = self._pebble_layer()
-        try:
-            plan = self._container.get_plan()
-            if plan.services != pebble_layer.services:
-                self._container.add_layer(self._container_name, pebble_layer, combine=True)
-                self._container.restart(self._service_name)
-                logger.info(f"Restarted container {self._service_name}")
-                self.unit.status = ActiveStatus()
-        except ConnectionError:
-            logger.error(
-                f"Could not restart {self._service_name} -- Pebble socket does "
-                f"not exist or is not responsive"
-            )
-
-    @property
-    def _namespace(self) -> str:
-        return self.model.name
+        self._orc8r_base = Orc8rBase(self, startup_command=startup_command)
 
 
 if __name__ == "__main__":
