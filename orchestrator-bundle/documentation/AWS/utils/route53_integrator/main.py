73cf89e0e01ad3a92f9697a2957a8867a5f6b3af
guillaume <guillaume.belanger27@gmail.com>
2022-02-23 09:38:24 -0500

    - Improves route53 integrator script documentation

--- orchestrator-bundle/documentation/AWS/utils/route53_integrator/main.py
+++ orchestrator-bundle/documentation/AWS/utils/route53_integrator/main.py
@@ -4,20 +4,23 @@
 
 import argparse
 import logging
+from dataclasses import dataclass
+
+from tabulate import tabulate
 
 from aws.aws import AWS
 from k8s.k8s import K8s
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format='[%(asctime)s] %(levelname)s - %(message)s'
 )
 
 
 DESCRIPTION = """
-Script that communicates with AWS and creates the following objects in Route53. Those objects
-are a hosted zone and four A records based on Kubernetes LoadBalancer services. For this script
-to work correctly, aws cli and kubectl must already be configured.
+Script that communicates with AWS Route53 and creates a hosted zone and four A records based on 
+four Kubernetes LoadBalancer services. For this script to work correctly, aws cli and kubectl must 
+already be configured.
 """
 
 
@@ -39,32 +42,81 @@ def parse_arguments() -> (str, str):
     return args.hosted_zone, args.namespace
 
 
+@dataclass
+class ARecord:
+    kubernetes_service: str
+    a_record_name: str
+    load_balancer_address: str = ""
+    elb_hosted_zone: str = ""
+
+
+def print_disclaimer(hosted_zone: str, a_records: [ARecord]):
+    headers = ["A Record Name", "LoadBalancer Address", "Kubernetes Service"]
+    data_list = [
+        [
+            a_records[i].a_record_name,
+            a_records[i].load_balancer_address,
+            a_records[i].kubernetes_service,
+        ]
+        for i in range(len(a_records))
+    ]
+    print(f"\nThe hosted zone {hosted_zone} will be created with the following A records:")
+    print("\n")
+    print(tabulate(data_list, headers=headers))
+    input("\nPress Enter to continue...")
+
+
+def print_nameservers(nameservers: [str]):
+    nameserver_list = []
+    for nameserver in nameservers:
+        nameserver_list.append([nameserver])
+    print("\nDone! Configure your domain registrar to use those nameservers:")
+    print(tabulate(nameserver_list))
+
+
 def main():
     hosted_zone, namespace = parse_arguments()
     aws = AWS()
     k8s = K8s(namespace)
+    a_records = [
+        ARecord(
+            kubernetes_service="nginx-proxy",
+            a_record_name=f"*.nms.{hosted_zone}",
+        ),
+        ARecord(
+            kubernetes_service="orc8r-bootstrap-nginx",
+            a_record_name=f"bootstrapper-controller.{hosted_zone}",
+        ),
+        ARecord(
+            kubernetes_service="orc8r-clientcert-nginx",
+            a_record_name=f"controller.{hosted_zone}",
+        ),
+        ARecord(
+            kubernetes_service="orc8r-nginx-proxy",
+            a_record_name=f"api.{hosted_zone}",
+        ),
+    ]
+
+    for a_record in a_records:
+        load_balancer_address = k8s.get_service_load_balancer_address(a_record.kubernetes_service)
+        elb_hosted_zone = aws.elb.get_hosted_zone_id(load_balancer_address)
+        a_record.load_balancer_address = load_balancer_address
+        a_record.elb_hosted_zone = elb_hosted_zone
+
+    print_disclaimer(hosted_zone, a_records)
 
     route53_zone = aws.route53.create_hosted_zone_if_doesnt_exist(hosted_zone)
     route53_zone_id = route53_zone["HostedZone"]["Id"].split("/")[-1]
 
-    service_mapping = {
-        "nginx-proxy": f"*.nms.{hosted_zone}",
-        "orc8r-bootstrap-nginx": f"bootstrapper-controller.{hosted_zone}",
-        "orc8r-clientcert-nginx": f"controller.{hosted_zone}",
-        "orc8r-nginx-proxy": f"api.{hosted_zone}",
-    }
-    for service in service_mapping:
-        load_balancer_address = k8s.get_service_load_balancer_address(service)
-        elb_hosted_zone = aws.elb.get_hosted_zone_id(load_balancer_address)
+    for a_record in a_records:
         aws.route53.create_alias_resource_a_record(
-            name=service_mapping[service],
-            alias_target_dns_name=load_balancer_address,
-            alias_target_hosted_zone_id=elb_hosted_zone,
+            name=a_record.a_record_name,
+            alias_target_dns_name=a_record.load_balancer_address,
+            alias_target_hosted_zone_id=a_record.elb_hosted_zone,
             hosted_zone_id=route53_zone_id
         )
     nameservers = route53_zone["DelegationSet"]["NameServers"]
-    logging.info("Done!")
-    logging.info(f"Nameservers: {nameservers}")
+    print_nameservers(nameservers)
 
 
 if __name__ == "__main__":
