e3533b624daded5bc849d6824c4e1143de952b70
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-05-31 12:16:38 +0200

    Addresses PR comments + static and lint issues

--- orchestrator-bundle/orc8r-accessd-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-accessd-operator/tests/integration/test_integration.py
@@ -23,6 +23,7 @@ class TestOrc8rAccessd:
         await ops_test.model.wait_for_idle(apps=["postgresql-k8s"], status="active", timeout=1000)
 
     @pytest.fixture(scope="module")
+    @pytest.mark.abort_on_fail
     async def build_and_deploy_charm(self, ops_test):
         charm = await ops_test.build_charm(".")
         resources = {
@@ -35,7 +36,6 @@ class TestOrc8rAccessd:
     async def test_wait_for_blocked_status(self, ops_test, build_and_deploy_charm):
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
 
-    @pytest.mark.abort_on_fail
     async def test_relate_and_wait_for_idle(self, ops_test, setup, build_and_deploy_charm):
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
         await ops_test.model.add_relation(
