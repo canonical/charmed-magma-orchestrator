9b486dc3371b3cd224ab6afb8b6e381d42152597
guillaume <guillaume.belanger27@gmail.com>
2022-06-03 16:42:25 -0400

    Adds vault-k8s to bundle and bumps the lib version to latest

--- orchestrator-bundle/orc8r-certifier-operator/lib/charms/tls_certificates_interface/v0/tls_certificates.py
+++ orchestrator-bundle/orc8r-certifier-operator/lib/charms/tls_certificates_interface/v0/tls_certificates.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-""" Library for the tls-certificates relation
+"""Library for the tls-certificates relation.
 
 This library contains the Requires and Provides classes for handling
 the tls-certificates interface.
@@ -97,16 +97,14 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 18
 
 REQUIRER_JSON_SCHEMA = {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "type": "object",
-    "examples": [
-        {
-            "cert_requests": [{"common_name": "whatever.com"}],
-        }
-    ],
+    "description": "The root schema comprises the entire JSON document. It contains the data bucket content and format for the requirer of the tls-certificates relation to ask TLS certificates to the provider.",  # noqa: E501
+    "examples": [{"cert_requests": [{"common_name": "canonical.com"}]}],
+    "properties": {"common_name": {"type": "string"}, "sans": {"type": "array"}},
     "anyOf": [
         {
             "required": ["cert_requests"],
@@ -148,49 +146,32 @@ REQUIRER_JSON_SCHEMA = {
 PROVIDER_JSON_SCHEMA = {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "type": "object",
-    "description": "The root schema comprises the entire JSON document. It contains the data "
-    "bucket content and format for the provider of the tls-certificates relation "
-    "to provide certificates to the requirer.",
-    "examples": [
+    "description": "The root schema comprises the entire JSON document. It contains the data bucket content and format for the provider of the tls-certificates relation to provide certificates to the requirer.",  # noqa: E501
+    "example": [
         {
-            "certificates": [
-                {
-                    "common_name": "banana.com",
-                    "key": "afaefawfawfawfaafe.key",
-                    "cert": "abavab.crt",
-                    "ca": "aefawe",
-                },
-                {
-                    "common_name": "pizza.com",
-                    "key": "aaaaaaa.key",
-                    "cert": "aaa.crt",
-                    "ca": "bbbbada",
-                },
-            ],
+            "whatever.com": {
+                "key": "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEA1CHw0NwHmuu/1ych0G/vlE0ArsPo3meWvG5u0rlg1xoGwdbh\nJeG7DIDtBlU71NFVs/DLdAtx2MwWg0rKYXGzBex+XaI4WLowLmD+KTk8ZCWvBHXq\nQ+N5Tc7CFlBnVW7xPqAE/HGFTgAhH+vq1lYKfXNpJMlXf3EuaNycmVwJgyKdh9d8\njzEqa54YWtH/E2guv+Rb9atGdmVHgLhNPgcwIuIODYJpQLjawnT2Lf8Z7MdTqVdV\n7slorOnz2mHslqC5KPnvDXIrVmrwhltf9DcWpxH8ZzkAfOdEFA53ioBBwaIPYoy1\nUvBJRnrbjhVoTq6XUM8BheqzfPppKnpnJPE2jwIDAQABAoIBAAjN39DLUQV9A1lK\npnygKLFfAMhAGUohwn/PlYnpZ7uFuQISiQWpeLnsH+pDX1hV19jABbGrR+5Xiheo\n4v1oWqXESvpX4T7Ne3JxVBsh5P/DEKB+xFpM9pvkGOoULDW/hQO0YICZtY6nMrjA\ncd6zc3wBbju4n4kKiYKQpW84Aq0Oj1OXIfo48F776MTm3WNAgdpHC5CYmQdUu20A\nAnmS52XSOcva9dZrs29kQM7iA2ssefLy+yQLR5jCiLvrmvjZtSCpDryfDC2A2nny\n4dMhyLCezTVcCy8kTTTgaOh+kxgbCwQJNT6xcOIB4fYBVUn0wd+UiCqxGVIehSGd\nLSlyMAECgYEA/Eax/FJ3SQa28z+to3i7zy03f2porDs+uKVyQcfZlxniEzoEKYPJ\nFdG4XtCjgre5YCUPeWCJgUEcc2x5XdP4T33bN9cMOgz+g4hpP6I2wj9/QV3tnAFL\nhlOIFEkDlBIURXIORU115D2CNBUWfGQAsJQeKE4YncaTWO9Gn08oAEECgYEA10OM\n4f31HmO/QyW3uARIgTF2GZDkVPeYvuG6DBDHGznJzdSNJgIP+Jotg344vR555IMQ\nUtvzg6cVpLdJypOI+sNiiYBlHUgyc4E0yGnBedNKpxF1gYw4sljX6fJ6FPaBcq9X\n6sKXC90KWe4lAvLE4c+qGInFqPxw7BPVBnV4gs8CgYB39jYUwjIu6557tUAgh/zm\n252UXUlA/TsqGqJmXV4+1/QFKIVqKFyqn3uIurXGJw9jhLwC/8DjUc7xpBdiYrWl\nNzfTKdOKlzs/2NITjFN1szQUJVIj6Qm86mO/Iakt9BrnmwDmO5tf2U/c7Fow9GzP\nit98UwapoA/ZLo7qmn1vAQKBgH+YHKO/4lD3EuF8M9+xOkDJzpTs20q50CIkriCE\nuWAb6tBEUr3arxjOWnf8kykWLW4TedODaF365ctSkTywIptwwLF8F3M53h200lKQ\nzQunADLzGFGHifu8yY50GYTfcG9IG7adTObNSFtx2yJaP/URIGOXFkBKEaz9PGGt\ns5blAoGBAI20XD6yAL8cD5EN0pZ2eHDQbF/g8ML8zxJG4vCV2ElhfH+L4Q2zXAYp\nDAGRj+z928KUM+OIjyts7RWoQm0/5Bf9VuvrC0o3H0pw4rmNyW1VEJWR7LLgA647\n7O8CvHALrL7aMh6XWNmDMSrO63nYN5JzRpxJXXtPqmqNSd6cUewP\n-----END RSA PRIVATE KEY-----",  # noqa: E501
+                "cert": "-----BEGIN CERTIFICATE-----\nMIIDajCCAlKgAwIBAgIUaKRAIcZmkNziPb6FpgfShKTHj/wwDQYJKoZIhvcNAQEL\nBQAwPTE7MDkGA1UEAxMyVmF1bHQgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkg\nKGNoYXJtLXBraS1sb2NhbCkwHhcNMjIwNTI2MDAzMzI0WhcNMjMwNTI1MjMzMzU0\nWjAVMRMwEQYDVQQDEwpibGFibGEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A\nMIIBCgKCAQEA1CHw0NwHmuu/1ych0G/vlE0ArsPo3meWvG5u0rlg1xoGwdbhJeG7\nDIDtBlU71NFVs/DLdAtx2MwWg0rKYXGzBex+XaI4WLowLmD+KTk8ZCWvBHXqQ+N5\nTc7CFlBnVW7xPqAE/HGFTgAhH+vq1lYKfXNpJMlXf3EuaNycmVwJgyKdh9d8jzEq\na54YWtH/E2guv+Rb9atGdmVHgLhNPgcwIuIODYJpQLjawnT2Lf8Z7MdTqVdV7slo\nrOnz2mHslqC5KPnvDXIrVmrwhltf9DcWpxH8ZzkAfOdEFA53ioBBwaIPYoy1UvBJ\nRnrbjhVoTq6XUM8BheqzfPppKnpnJPE2jwIDAQABo4GJMIGGMA4GA1UdDwEB/wQE\nAwIDqDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwHQYDVR0OBBYEFASO\nWjQVWEhsucKdwUeyq4RRwmygMB8GA1UdIwQYMBaAFPOJYJ5nPJg3UVlfKPxggdig\n/n+nMBUGA1UdEQQOMAyCCmJsYWJsYS5jb20wDQYJKoZIhvcNAQELBQADggEBAGq8\nVrNFmTkf9jG3R8yD1HIZp0cbDacF25SHSYS3+M32BlITve0OOA0CzW3OLrXnCTp7\nLoSMWpWM5TFeJNl/lV4bC8izXA3hsf3bHXERkEGfjuTUmjK8QodvAs/ueoaD1E/Y\n0b9w3Qb3+dbs9joU/2XltvOcTPmtjTsfkMQ12sFozzLn4LVZTDe9Pmt2YXcnv+nd\navU0bCVNWYLc/6AHImtKYrziBBk+mfwYkPFFdwjpwHVPuCTMsZSBY8TrSuuk79w4\nBTRXzEBsCizprRGFRmZnFCA+SbMkh2PWpKziujODdGMZjUWtgFI2AGwMtIk2KXLK\nD/8jxyBXHnvBJ5S3vNo=\n-----END CERTIFICATE-----",  # noqa: E501
+            },
+            "ca": "-----BEGIN CERTIFICATE-----\nMIIDazCCAlOgAwIBAgIUcmcehy58Qe+g00FUC4WrGYqOBwwwDQYJKoZIhvcNAQEL\nBQAwPTE7MDkGA1UEAxMyVmF1bHQgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkg\nKGNoYXJtLXBraS1sb2NhbCkwHhcNMjIwNTI2MDAxNzIwWhcNMzIwNTIyMjMxNzUw\nWjA9MTswOQYDVQQDEzJWYXVsdCBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eSAo\nY2hhcm0tcGtpLWxvY2FsKTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\nAMQ5VG4o/tKjAr+1p0fH4XP+IPlGJTubDuMC1S7KwwxqyXmW/PXhIxC4zPPiRodK\nJ1sBtAla0fVQ5xRjou/FTdKtHbXMm9Nh4fVauFu7HMLHvDjBwZKl26eZ536QhOO9\nggjs/Gx9pYoWmTKqDGbGZRgKx4zjYmMNYdY7VfpLqmzYEMMPy4mQRHajWo7ksdMh\nev/ZU0PSyY4Vfk7+a8O/gTGebRYZzUAVMJX+7RMySqPvtI7Cm9KrYZFLDoLT/yKW\neOi/oynbCTnCKllR9GsmjHtO/bjoE4Ggmn4zWHSZSaoe7deG27CVHT/gU+DLQhQi\n/MNbCItkUzYKU6YO+0INXp8CAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgEGMA8GA1Ud\nEwEB/wQFMAMBAf8wHQYDVR0OBBYEFPOJYJ5nPJg3UVlfKPxggdig/n+nMB8GA1Ud\nIwQYMBaAFPOJYJ5nPJg3UVlfKPxggdig/n+nMA0GCSqGSIb3DQEBCwUAA4IBAQAj\nHzDsi3GNtp6mPAt9eUjR69WPdS8GgI4ypqIaKjS/r8lwEB1y9FT50NgYhb+nH/2y\nj5ajQEF/Mf9GBJOpFtPWpULxPra5EeCVpMS9sCP1BFS3Tq1/p09kb5kGNzJPQ5u1\nNJDJMAhzUHZcxCnqNBrRKhvtWKNZygvcZuV2nypN+vvtMXlZv5GMrYGpOUomUGza\nviGfaLiGdNeWBXElKe1slutUXXTkLOMS7rLQ5RziDrVxXn9uuE1lrTovEACvrP1Z\n0BFJIuGTn699OGevx44u4gO4qIkzpGeQ1gAnSdgq2HZpxSAYdi2ay8MSHbOfsLY0\nAIdVl46lbjsmIh+vvFyI\n-----END CERTIFICATE-----",  # noqa: E501
+            "chain": "-----BEGIN CERTIFICATE-----\nMIIDazCCAlOgAwIBAgIUcmcehy58Qe+g00FUC4WrGYqOBwwwDQYJKoZIhvcNAQEL\nBQAwPTE7MDkGA1UEAxMyVmF1bHQgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkg\nKGNoYXJtLXBraS1sb2NhbCkwHhcNMjIwNTI2MDAxNzIwWhcNMzIwNTIyMjMxNzUw\nWjA9MTswOQYDVQQDEzJWYXVsdCBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eSAo\nY2hhcm0tcGtpLWxvY2FsKTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\nAMQ5VG4o/tKjAr+1p0fH4XP+IPlGJTubDuMC1S7KwwxqyXmW/PXhIxC4zPPiRodK\nJ1sBtAla0fVQ5xRjou/FTdKtHbXMm9Nh4fVauFu7HMLHvDjBwZKl26eZ536QhOO9\nggjs/Gx9pYoWmTKqDGbGZRgKx4zjYmMNYdY7VfpLqmzYEMMPy4mQRHajWo7ksdMh\nev/ZU0PSyY4Vfk7+a8O/gTGebRYZzUAVMJX+7RMySqPvtI7Cm9KrYZFLDoLT/yKW\neOi/oynbCTnCKllR9GsmjHtO/bjoE4Ggmn4zWHSZSaoe7deG27CVHT/gU+DLQhQi\n/MNbCItkUzYKU6YO+0INXp8CAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgEGMA8GA1Ud\nEwEB/wQFMAMBAf8wHQYDVR0OBBYEFPOJYJ5nPJg3UVlfKPxggdig/n+nMB8GA1Ud\nIwQYMBaAFPOJYJ5nPJg3UVlfKPxggdig/n+nMA0GCSqGSIb3DQEBCwUAA4IBAQAj\nHzDsi3GNtp6mPAt9eUjR69WPdS8GgI4ypqIaKjS/r8lwEB1y9FT50NgYhb+nH/2y\nj5ajQEF/Mf9GBJOpFtPWpULxPra5EeCVpMS9sCP1BFS3Tq1/p09kb5kGNzJPQ5u1\nNJDJMAhzUHZcxCnqNBrRKhvtWKNZygvcZuV2nypN+vvtMXlZv5GMrYGpOUomUGza\nviGfaLiGdNeWBXElKe1slutUXXTkLOMS7rLQ5RziDrVxXn9uuE1lrTovEACvrP1Z\n0BFJIuGTn699OGevx44u4gO4qIkzpGeQ1gAnSdgq2HZpxSAYdi2ay8MSHbOfsLY0\nAIdVl46lbjsmIh+vvFyI\n-----END CERTIFICATE-----",  # noqa: E501
+            "unit_name": "whatever_unit_name",
         }
     ],
     "properties": {
-        "certificates": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "common_name": {"type": "string"},
-                    "key": {"type": "string"},
-                    "cert": {"type": "string"},
-                    "ca": {"type": "string"},
-                },
-                "required": ["common_name", "key", "cert", "ca"],
-            },
-        }
+        "ca": {"type": "string"},
+        "chain": {"type": "string"},
+        "unit_name": {"type": "string"},
     },
-    "required": ["certificates"],
+    "required": ["ca", "chain"],
 }
 
 logger = logging.getLogger(__name__)
 
 
 class Cert(TypedDict):
+    """Certificate data object."""
+
     common_name: str
     cert: str
     key: str
@@ -198,18 +179,24 @@ class Cert(TypedDict):
 
 
 class CertificateAvailableEvent(EventBase):
+    """Charm Event triggered when a TLS certificate is available."""
+
     def __init__(self, handle, certificate_data: Cert):
         super().__init__(handle)
         self.certificate_data = certificate_data
 
-    def snapshot(self):
+    def snapshot(self) -> dict:
+        """Returns snapshot."""
         return {"certificate_data": self.certificate_data}
 
-    def restore(self, snapshot):
+    def restore(self, snapshot: dict):
+        """Restores snapshot."""
         self.certificate_data = snapshot["certificate_data"]
 
 
 class CertificateRequestEvent(EventBase):
+    """Charm Event triggered when a TLS certificate is required."""
+
     def __init__(self, handle, common_name: str, sans: str, cert_type: str, relation_id: int):
         super().__init__(handle)
         self.common_name = common_name
@@ -217,7 +204,8 @@ class CertificateRequestEvent(EventBase):
         self.cert_type = cert_type
         self.relation_id = relation_id
 
-    def snapshot(self):
+    def snapshot(self) -> dict:
+        """Returns snapshot."""
         return {
             "common_name": self.common_name,
             "sans": self.sans,
@@ -225,7 +213,8 @@ class CertificateRequestEvent(EventBase):
             "relation_id": self.relation_id,
         }
 
-    def restore(self, snapshot):
+    def restore(self, snapshot: dict):
+        """Restores snapshot."""
         self.common_name = snapshot["common_name"]
         self.sans = snapshot["sans"]
         self.cert_type = snapshot["cert_type"]
@@ -243,14 +232,19 @@ def _load_relation_data(raw_relation_data: dict) -> dict:
 
 
 class CertificatesProviderCharmEvents(CharmEvents):
+    """List of events that the TLS Certificates provider charm can leverage."""
+
     certificate_request = EventSource(CertificateRequestEvent)
 
 
 class CertificatesRequirerCharmEvents(CharmEvents):
+    """List of events that the TLS Certificates requirer charm can leverage."""
+
     certificate_available = EventSource(CertificateAvailableEvent)
 
 
 class InsecureCertificatesProvides(Object):
+    """TLS certificates provider class to be instantiated by TLS certificates providers."""
 
     on = CertificatesProviderCharmEvents()
 
@@ -264,8 +258,8 @@ class InsecureCertificatesProvides(Object):
 
     @staticmethod
     def _relation_data_is_valid(certificates_data: dict) -> bool:
-        """
-        Uses JSON schema validator to validate relation data content.
+        """Uses JSON schema validator to validate relation data content.
+
         :param certificates_data: Certificate data dictionary as retrieved from relation data.
         :return: True/False depending on whether the relation data follows the json schema.
         """
@@ -276,24 +270,28 @@ class InsecureCertificatesProvides(Object):
             return False
 
     def set_relation_certificate(self, certificate: Cert, relation_id: int):
+        """Adds certificates to relation data.
+
+        :param certificate: Certificate object
+        :param relation_id: Relation ID
+        """
         logging.info(f"Setting Certificate to {certificate} for {self.model.unit}")
         certificates_relation = self.model.get_relation(
             relation_name=self.relationship_name, relation_id=relation_id
         )
-        current_certificates_json_dump = certificates_relation.data[self.model.unit].get(  # type: ignore[union-attr]  # noqa: E501
-            "certificates"
-        )
-        current_certificate_list = []
-        if current_certificates_json_dump:
-            current_certificate_list = json.loads(current_certificates_json_dump)
-            for cert in current_certificate_list:
-                if cert["common_name"] == certificate["common_name"]:
-                    logger.info("Certificate with the same common name already existed")
-                    return
-        current_certificate_list.append(certificate)
-        certificates_relation.data[self.model.unit]["certificates"] = json.dumps(  # type: ignore[union-attr]  # noqa: E501
-            current_certificate_list
-        )
+        relation_data = certificates_relation.data[self.model.unit]  # type: ignore[union-attr]
+
+        current_ca = relation_data.get("ca")
+        current_chain = relation_data.get("chain")
+        if not current_ca:
+            relation_data["ca"] = certificate["ca"]
+        if not current_chain:
+            relation_data["chain"] = certificate["ca"]
+        certificate_dict = {"key": certificate["key"], "cert": certificate["cert"]}
+        relation_data[certificate["common_name"]] = json.dumps(certificate_dict)
+
+        certificate_dict = {"key": certificate["key"], "cert": certificate["cert"]}
+        relation_data[certificate["common_name"]] = json.dumps(certificate_dict)
 
     def _on_relation_changed(self, event):
         logger.info(f"Raw relation data: {event.relation.data}")
@@ -324,6 +322,7 @@ class InsecureCertificatesProvides(Object):
 
 
 class InsecureCertificatesRequires(Object):
+    """TLS certificates requirer class to be instantiated by TLS certificates requirers."""
 
     on = CertificatesRequirerCharmEvents()
 
@@ -349,6 +348,15 @@ class InsecureCertificatesRequires(Object):
         common_name: str,
         sans: list = None,
     ):
+        """Request TLS certificate to provider charm.
+
+        :param cert_type: Certificate type: "client" or "server". Specifies if certificates are
+            flagged for server or client authentication use. See RFC 5280 Section 4.2.1.12 for
+            information about the Extended Key Usage field.
+        :param common_name: The requested CN for the certificate.
+        :param sans: Subject Alternative Name
+        :return: None
+        """
         if not sans:
             sans = []
         logger.info("Received request to create certificate")
@@ -384,6 +392,21 @@ class InsecureCertificatesRequires(Object):
         except exceptions.ValidationError:
             return False
 
+    @staticmethod
+    def _parse_certificates_from_relation_data(relation_data: dict) -> list:
+        certificates = []
+        ca = relation_data.pop("ca")
+        relation_data.pop("chain")
+        for key in relation_data:
+            if type(relation_data[key]) == dict:
+                private_key = relation_data[key].get("key")
+                certificate = relation_data[key].get("cert")
+                if private_key and certificate:
+                    certificates.append(
+                        Cert(common_name=key, key=private_key, cert=certificate, ca=ca)
+                    )
+        return certificates
+
     def _on_relation_changed(self, event):
         if self.model.unit.is_leader():
             logger.info(f"Raw relation data: {event.relation.data}")
@@ -393,5 +416,7 @@ class InsecureCertificatesRequires(Object):
                 logger.info("Relation data did not pass JSON Schema validation - Deferring")
                 event.defer()
                 return
-            for certificate in relation_data["certificates"]:
+
+            certificates = self._parse_certificates_from_relation_data(relation_data)
+            for certificate in certificates:
                 self.on.certificate_available.emit(certificate_data=certificate)
