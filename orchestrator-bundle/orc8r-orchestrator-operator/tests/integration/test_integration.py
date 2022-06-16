a15fd921875ee612c720e07065d58c905f0ef6a6
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-06-16 16:05:48 +0200

    Fix: relate call at wrong place

--- orchestrator-bundle/orc8r-orchestrator-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-orchestrator-operator/tests/integration/test_integration.py
@@ -67,13 +67,12 @@ class TestOrchestrator:
             charm, resources=resources, application_name=APPLICATION_NAME, trust=True
         )
 
-        await ops_test.model.add_relation(
-            relation1=APPLICATION_NAME, relation2="orc8r-certifier:magma-orc8r-certifier"
-        )
-
     @pytest.mark.abort_on_fail
     async def test_wait_for_blocked_status(self, ops_test, setup, build_and_deploy):
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
 
-    async def test_wait_for_idle(self, ops_test, setup, build_and_deploy):
+    async def test_relate_and_wait_for_idle(self, ops_test, setup, build_and_deploy):
+        await ops_test.model.add_relation(
+            relation1=APPLICATION_NAME, relation2="orc8r-certifier:magma-orc8r-certifier"
+        )
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
