b7bc9897660d5610e78759e12fcdee7c4b239465
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2022-06-23 16:17:09 +0200

    Updating versions since the old ones were already taken

--- orchestrator-bundle/orc8r-libs/lib/charms/magma_orc8r_libs/v0/orc8r_base.py
+++ orchestrator-bundle/orc8r-libs/lib/charms/magma_orc8r_libs/v0/orc8r_base.py
@@ -66,7 +66,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 11
+LIBPATCH = 12
 
 
 logger = logging.getLogger(__name__)
