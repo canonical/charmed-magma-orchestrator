fe81d13bedfff1404a5265326f0f272d489d6e49
Ubuntu <ubuntu@ip-172-31-38-85.eu-west-1.compute.internal>
2022-06-27 10:15:07 +0000

    Fix: Now deploys local charms

--- orchestrator-bundle/orc8r-bundle/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-bundle/tests/integration/test_integration.py
@@ -8,34 +8,26 @@ from pathlib import Path
 import pytest
 from pytest_operator.plugin import OpsTest  # type: ignore[import]  # noqa: F401
 
-from juju.controller import Controller
+from helpers import (cli_deploy_bundle)
 
 
 logger = logging.getLogger(__name__)
 
 
 class TestOrc8rBundle:
-    async def cli_deploy_bundle(ops_test: OpsTest, name: str, channel: str = "edge"):
-        run_args = [
-            "juju",
-            "deploy",
-            "./bundle-local.yaml",
-            "--trust",
-            "-m",
-            ops_test.model_full_name,
-        ]
-
-        retcode, stdout, stderr = await ops_test.run(*run_args)
-        logger.info("Deploying bundle")
-        logger.info(stdout)
-
     @pytest.fixture(scope="module")
     @pytest.mark.abort_on_fail
     async def deploy_bundle(self, ops_test: OpsTest):
+        run_args = [
+            "juju",
+            "deploy",
+            "/home/ubuntu/charmed-magma/orchestrator-bundle/bundle-local.yaml",
+            "--trust",
+        ]
+
         logger.info("Deploying bundle")
-        await cli_deploy_bundle(ops_test, "magma-orc8r")
-        logger.info("Bundle deployed!")
-        
-    
+        retcode, stdout, stderr = await ops_test.run(*run_args)
+        logger.info(stdout)
+
     async def test_wait_for_idle(self, ops_test: OpsTest, deploy_bundle):
         await ops_test.model.wait_for_idle(status="active", timeout=1500)
