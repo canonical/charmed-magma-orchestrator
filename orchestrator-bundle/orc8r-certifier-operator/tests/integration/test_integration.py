1937b87c908140e2a7f98addbb9bca97fce1c2dd
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2022-06-28 13:34:26 +0200

    Test

--- orchestrator-bundle/orc8r-certifier-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-certifier-operator/tests/integration/test_integration.py
@@ -27,6 +27,9 @@ class TestOrc8rCertifier:
     @pytest.mark.abort_on_fail
     async def build_and_deploy(self, ops_test, setup):
         charm = await ops_test.build_charm(".")
+        print("==================================================================")
+        print(charm)
+        print("==================================================================")
         resources = {
             f"{CHARM_NAME}-image": METADATA["resources"][f"{CHARM_NAME}-image"]["upstream-source"],
         }
