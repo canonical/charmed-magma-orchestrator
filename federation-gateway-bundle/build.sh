61f865c842166e1703f84feea6e33b660f787e72
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-07-11 10:51:45 +0200

    Adds automation scripts and renames charms adding operator sufix

---
+++ federation-gateway-bundle/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function build() {
+  charm="$1"
+    pushd "federation-gateway-bundle/${charm}-operator/"
+    charmcraft pack
+    mv -f "magma-${charm}_ubuntu-20.04-amd64.charm" "${charm}.charm"
+    popd
+}
+
+charms="
+feg-eap-sim
+feg-health
+feg-hello
+feg-radius
+feg-s8-proxy
+feg-session-proxy
+"
+
+for charm in ${charms}; do
+    build ${charm}
+done
+
+wait
