710b0beda4231713125b3e4ca3a46c9896cfecbf
Guillaume Belanger <guillaume.belanger@canonical.com>
2022-07-19 08:52:23 -0400

    Update orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
    Co-authored-by: Ghislain Bourgeois <ghislain.bourgeois@canonical.com>

--- orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
@@ -44,7 +44,7 @@ class TestOrc8rNginx:
         await self._deploy_orc8r_obsidian(ops_test)
 
     @staticmethod
-    def _find_charm(charm_dir: str, charm_file_name: str) -> Union[str, None]:
+    def _find_charm(charm_dir: str, charm_file_name: str) -> Optional[str]:
         for root, _, files in os.walk(charm_dir):
             for file in files:
                 if file == charm_file_name:
