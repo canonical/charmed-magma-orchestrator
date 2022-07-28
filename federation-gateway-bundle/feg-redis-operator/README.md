ba2ddbe5b1408dadf9a8cf4cf0932b563632c3c2
Javier <ubuntu@ip-172-31-38-85.eu-west-1.compute.internal>
2022-07-28 07:35:10 +0000

    Feat: Adds feg-redis-operator

---
+++ federation-gateway-bundle/feg-redis-operator/README.md
@@ -0,0 +1,17 @@
+# feg-redis-operator
+
+## Description
+
+Federation Gateway redis service.
+
+## Usage
+
+Deploy using Juju:
+
+```bash
+juju deploy magma-feg-redis  feg-redis --trust
+```
+
+## OCI Images
+
+Default: docker.artifactory.magmacore.org/gateway_python:latest
---
+++ federation-gateway-bundle/feg-redis-operator/tests/unit/test_charm.py
@@ -0,0 +1,49 @@
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import unittest
+
+from ops import testing
+from ops.model import ActiveStatus
+
+from charm import FegRedisCharm
+
+testing.SIMULATE_CAN_CONNECT = True
+
+
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.container_name = "magma-feg-redis"
+        self.harness = testing.Harness(FegRedisCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_given_can_connect_to_container_when_pebble_ready_then_status_is_active(self):
+
+        self.harness.container_pebble_ready(container_name=self.container_name)
+
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+
+    def test_given_can_connect_to_container_when_pebble_ready_then_redis_service_added_to_pebble_plan(  # noqa: E501
+        self,
+    ):
+
+        self.harness.container_pebble_ready(container_name=self.container_name)
+
+        expected_plan = {
+            "services": {
+                "magma-feg-redis": {
+                    "override": "replace",
+                    "summary": "magma-feg-redis",
+                    "command": "/bin/bash -c "
+                    "'/usr/local/bin/generate_service_config.py "
+                    "--service=redis --template=redis &&\n "
+                    "/usr/bin/redis-server /var/opt/magma/tmp/redis.conf "
+                    "--daemonize no &&\n /usr/bin/redis-cli shutdown’",
+                    "startup": "enabled",
+                }
+            },
+        }
+        updated_plan = self.harness.get_container_pebble_plan("magma-feg-redis").to_dict()
+
+        self.assertEqual(expected_plan, updated_plan)
