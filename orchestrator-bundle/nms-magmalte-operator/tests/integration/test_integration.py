8a35af86fb4723165e5fada59f64f482ca815ea6
yazansalti <yazan.salti@canonical.com>
2022-09-27 17:12:41 +0200

    Fixes integration tests

--- orchestrator-bundle/nms-magmalte-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/nms-magmalte-operator/tests/integration/test_integration.py
@@ -94,15 +94,6 @@ class TestNmsMagmaLTE:
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
 
     async def test_relate_and_wait_for_idle(self, ops_test, setup, build_and_deploy_charm):
-        await ops_test.model.add_relation(
-            relation1=APPLICATION_NAME, relation2="postgresql-k8s:db"
-        )
-        await ops_test.model.add_relation(
-            relation1=APPLICATION_NAME, relation2="orc8r-certifier:cert-admin-operator"
-        )
-        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
-
-    async def test_db_relation_broken_then_joined(self, ops_test, setup, build_and_deploy_charm):
         rel_id = await ops_test.model.add_relation(
             relation1=APPLICATION_NAME, relation2="postgresql-k8s:db"
         )
