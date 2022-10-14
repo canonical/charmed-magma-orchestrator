c6f18c4cadb603768a78c8b0735d325c787532d0
saltiyazan <yazan.salti@canonical.com>
2022-10-14 08:16:55 +0000

    Fixes integration tests

--- orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
@@ -69,6 +69,7 @@ class TestOrc8rNginx:
             application_name=CERTIFIER_APPLICATION_NAME,
             config={"domain": DOMAIN},
             trust=True,
+            series="focal",
         )
         await ops_test.model.add_relation(
             relation1=CERTIFIER_APPLICATION_NAME, relation2="postgresql-k8s:db"
@@ -93,6 +94,7 @@ class TestOrc8rNginx:
             resources=resources,
             application_name=BOOTSTRAPPER_APPLICATION_NAME,
             trust=True,
+            series="focal",
         )
 
     async def _deploy_orc8r_obsidian(self, ops_test):
@@ -109,6 +111,7 @@ class TestOrc8rNginx:
             resources=resources,
             application_name=OBSIDIAN_APPLICATION_NAME,
             trust=True,
+            series="focal",
         )
 
     @staticmethod
@@ -136,6 +139,7 @@ class TestOrc8rNginx:
             application_name=APPLICATION_NAME,
             trust=True,
             config={"domain": DOMAIN},
+            series="focal",
         )
 
     @pytest.mark.abort_on_fail
