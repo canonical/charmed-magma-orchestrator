655c673431dd7ee48a1cbe0e8c96caa392951bdf
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2022-12-07 10:14:06 +0100

    Addressing PR comments

--- fluentd/tests/unit/test_charm.py
+++ fluentd/tests/unit/test_charm.py
@@ -21,69 +21,39 @@ VALID_TEST_CHARM_CONFIG: Mapping[str, Union[str, int]] = {
     "fluentd-chunk-limit-size": TEST_FLUENTD_CHUNK_LIMIT,
     "fluentd-queue-limit-length": TEST_FLUENTD_QUEUE_LIMIT,
 }
+TEST_OUTPUT_CONF_TEMPLATE = b"""{{ elasticsearch_host }}
+{{ elasticsearch_port }}
+{{ fluentd_chunk_limit_size }}
+{{ fluentd_queue_limit_length }}
+"""
 
 
 class TestCharm(unittest.TestCase):
     @patch(
         "charm.KubernetesServicePatch",
-        lambda charm, ports, service_type, service_name: None,
+        lambda charm, ports, service_type: None,
     )
     def setUp(self):
         self.harness = testing.Harness(FluentdElasticsearchCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
-
-    def test_given_elasticsearch_url_not_configured_when_pebble_ready_then_status_is_blocked(self):
         self.harness.container_pebble_ready(container_name="fluentd")
 
-        assert self.harness.charm.unit.status == BlockedStatus(
-            "Config for elasticsearch is not valid. Format should be <hostname>:<port>"
-        )
-
-    def test_given_invalid_elasticsearch_url_when_pebble_ready_then_status_is_blocked(self):
-        invalid_elasticsearch_url = "this is wrong"
-        config = {
-            "elasticsearch-url": invalid_elasticsearch_url,
-        }
-        self.harness.update_config(key_values=config)
-
-        self.harness.container_pebble_ready(container_name="fluentd")
-
-        assert self.harness.charm.unit.status == BlockedStatus(
-            "Config for elasticsearch is not valid. Format should be <hostname>:<port>"
-        )
-
-    @patch("ops.model.Container.exists")
     @patch("ops.model.Container.push")
     @patch("builtins.open", new_callable=mock_open())
-    def test_given_config_is_valid_when_pebble_ready_then_fluentd_configs_are_pushed_to_the_container(  # noqa: E501
-        self, patched_open, patched_push, patched_exists
+    def test_given_fluentd_charm_when_install_then_static_configs_are_pushed_to_the_container(
+        self, patched_open, patched_push
     ):
-        patched_exists.return_value = True
-        test_output_conf_template = b"""
-        {{ elasticsearch_host }}
-        {{ elasticsearch_port }}
-        {{ fluentd_chunk_limit_size }}
-        {{ fluentd_queue_limit_length }}
-        """
         test_fluentd_configs = [
             "config one",
             "config two",
             "config three",
-            test_output_conf_template,
         ]
         patched_open.side_effect = [
             mock_open(read_data=content).return_value for content in test_fluentd_configs
         ]
-        self.harness.update_config(key_values=VALID_TEST_CHARM_CONFIG)
-        expected_rendered_test_optput_conf = f"""
-        {TEST_ES_URL.split(":")[0]}
-        {TEST_ES_URL.split(":")[1]}
-        {TEST_FLUENTD_CHUNK_LIMIT}
-        {TEST_FLUENTD_QUEUE_LIMIT}
-        """
 
-        self.harness.container_pebble_ready(container_name="fluentd")
+        self.harness.charm.on.install.emit()
 
         patched_push.assert_has_calls(
             calls=[
@@ -102,12 +72,38 @@ class TestCharm(unittest.TestCase):
                     test_fluentd_configs[2],
                     permissions=0o777,
                 ),
-                call(
-                    Path("/etc/fluent/config.d/output.conf"),
-                    expected_rendered_test_optput_conf,
-                    permissions=0o777,
-                ),
             ]
+        )
+
+    def test_given_invalid_elasticsearch_url_when_pebble_ready_then_status_is_blocked(self):
+        invalid_elasticsearch_url = "this is wrong"
+        config = {
+            "elasticsearch-url": invalid_elasticsearch_url,
+        }
+        self.harness.update_config(key_values=config)
+
+        assert self.harness.charm.unit.status == BlockedStatus(
+            "Config for elasticsearch is not valid. Format should be <hostname>:<port>"
+        )
+
+    @patch("ops.model.Container.exists")
+    @patch("ops.model.Container.push")
+    @patch("builtins.open", new_callable=mock_open, read_data=TEST_OUTPUT_CONF_TEMPLATE)
+    def test_given_config_is_valid_when_pebble_ready_then_fluentd_configs_are_pushed_to_the_container(  # noqa: E501
+        self, _, patched_push, patched_exists
+    ):
+        patched_exists.return_value = True
+
+        self.harness.update_config(key_values=VALID_TEST_CHARM_CONFIG)
+        expected_rendered_test_optput_conf = f"""{TEST_ES_URL.split(":")[0]}
+{TEST_ES_URL.split(":")[1]}
+{TEST_FLUENTD_CHUNK_LIMIT}
+{TEST_FLUENTD_QUEUE_LIMIT}"""
+
+        patched_push.assert_called_once_with(
+            Path("/etc/fluent/config.d/output.conf"),
+            expected_rendered_test_optput_conf,
+            permissions=511,
         )
 
     @patch("ops.model.Container.exists")
@@ -117,8 +113,6 @@ class TestCharm(unittest.TestCase):
     ):
         patched_exists.return_value = True
         self.harness.update_config(key_values=VALID_TEST_CHARM_CONFIG)
-
-        self.harness.container_pebble_ready(container_name="fluentd")
 
         expected_plan = {
             "services": {
@@ -140,8 +134,7 @@ class TestCharm(unittest.TestCase):
         self, patched_exists
     ):
         patched_exists.return_value = True
-        self.harness.update_config(key_values=VALID_TEST_CHARM_CONFIG)
 
-        self.harness.container_pebble_ready(container_name="fluentd")
+        self.harness.update_config(key_values=VALID_TEST_CHARM_CONFIG)
 
         assert self.harness.charm.unit.status == ActiveStatus()
