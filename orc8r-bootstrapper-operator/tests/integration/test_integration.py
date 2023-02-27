92729f0b80a4b9348c818857501138f86b7dc6be
yazansalti <yazan.salti@canonical.com>
2023-02-27 16:48:13 +0400

    Fixes integration test

--- orc8r-bootstrapper-operator/tests/integration/test_integration.py
+++ orc8r-bootstrapper-operator/tests/integration/test_integration.py
@@ -3,19 +3,83 @@
 # See LICENSE file for licensing details.
 
 import logging
+import os
 from pathlib import Path
+from typing import Optional
 
 import pytest
 import yaml
 
 logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+CERTIFIER_METADATA = yaml.safe_load(Path("../orc8r-certifier-operator/metadata.yaml").read_text())
 
 APPLICATION_NAME = "orc8r-bootstrapper"
 CHARM_NAME = "magma-orc8r-bootstrapper"
+CERTIFIER_APPLICATION_NAME = "orc8r-certifier"
+CERTIFIER_CHARM_NAME = "magma-orc8r-certifier"
+CERTIFIER_CHARM_FILE_NAME = "magma-orc8r-certifier_ubuntu-20.04-amd64.charm"
+DB_APPLICATION_NAME = "postgresql-k8s"
 
 
 class TestOrc8rBootstrapper:
+    @pytest.fixture(scope="module")
+    @pytest.mark.abort_on_fail
+    async def setup(self, ops_test):
+        await self._deploy_postgresql(ops_test)
+        await self._deploy_tls_certificates_operator(ops_test)
+        await self._deploy_orc8r_certifier(ops_test)
+
+    @staticmethod
+    def _find_charm(charm_dir: str, charm_file_name: str) -> Optional[str]:
+        for root, _, files in os.walk(charm_dir):
+            for file in files:
+                if file == charm_file_name:
+                    return os.path.join(root, file)
+        return None
+
+    @staticmethod
+    async def _deploy_postgresql(ops_test):
+        await ops_test.model.deploy("postgresql-k8s", application_name=DB_APPLICATION_NAME)
+
+    @staticmethod
+    async def _deploy_tls_certificates_operator(ops_test):
+        await ops_test.model.deploy(
+            "tls-certificates-operator",
+            application_name="tls-certificates-operator",
+            config={
+                "generate-self-signed-certificates": True,
+                "ca-common-name": "rootca.whatever",
+            },
+            channel="edge",
+        )
+
+    async def _deploy_orc8r_certifier(self, ops_test):
+        certifier_charm = self._find_charm(
+            "../orc8r-certifier-operator", CERTIFIER_CHARM_FILE_NAME
+        )
+        if not certifier_charm:
+            certifier_charm = await ops_test.build_charm("../orc8r-certifier-operator/")
+        resources = {
+            f"{CERTIFIER_CHARM_NAME}-image": CERTIFIER_METADATA["resources"][
+                f"{CERTIFIER_CHARM_NAME}-image"
+            ]["upstream-source"],
+        }
+        await ops_test.model.deploy(
+            certifier_charm,
+            resources=resources,
+            application_name=CERTIFIER_APPLICATION_NAME,
+            config={"domain": "whatever"},
+            trust=True,
+            series="focal",
+        )
+        await ops_test.model.add_relation(
+            relation1=CERTIFIER_APPLICATION_NAME, relation2="postgresql-k8s:db"
+        )
+        await ops_test.model.add_relation(
+            relation1=CERTIFIER_APPLICATION_NAME, relation2="tls-certificates-operator"
+        )
+
     @pytest.fixture(scope="module")
     @pytest.mark.abort_on_fail
     async def build_and_deploy(self, ops_test):
@@ -31,8 +95,32 @@ class TestOrc8rBootstrapper:
             series="focal",
         )
 
-    @pytest.mark.abort_on_fail
-    async def test_wait_for_active_status(self, ops_test, build_and_deploy):
+    async def test_wait_for_blocked_status(self, ops_test, setup, build_and_deploy_charm):
+        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
+
+    async def test_relate_and_wait_for_idle(self, ops_test, setup, build_and_deploy_charm):
+        await ops_test.model.add_relation(
+            relation1=APPLICATION_NAME, relation2="postgresql-k8s:db"
+        )
+        await ops_test.model.add_relation(
+            relation1=APPLICATION_NAME, relation2="orc8r-certifier:cert-root-ca"
+        )
+        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+
+    async def test_remove_db_application(self, ops_test, setup, build_and_deploy_charm):
+        await ops_test.model.remove_application(
+            DB_APPLICATION_NAME, block_until_done=True, force=True
+        )
+        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
+
+    async def test_redeploy_db(self, ops_test, setup, build_and_deploy_charm):
+        await self._deploy_postgresql(ops_test)
+        await ops_test.model.add_relation(
+            relation1=CERTIFIER_APPLICATION_NAME, relation2="postgresql-k8s:db"
+        )
+        await ops_test.model.add_relation(
+            relation1=APPLICATION_NAME, relation2="postgresql-k8s:db"
+        )
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
 
     async def test_scale_up(self, ops_test, build_and_deploy):
