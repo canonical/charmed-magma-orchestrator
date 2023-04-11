c9ca4810d2d2e37523f20f4431f2d7bb7c6796b5
yazansalti <yazan.salti@canonical.com>
2023-04-11 15:02:26 +0400

    Fixes dependencies

--- shared_test_utils/integration_test_utils.py
+++ shared_test_utils/integration_test_utils.py
@@ -101,7 +101,7 @@ async def deploy_orc8r_certifier(ops_test):
         application_name=CERTIFIER_APPLICATION_NAME,
         config={"domain": "whatever.com"},
         trust=True,
-        series="focal",
+        series="jammy",
     )
     await ops_test.model.add_relation(
         relation1=CERTIFIER_APPLICATION_NAME, relation2="postgresql-k8s:db"
