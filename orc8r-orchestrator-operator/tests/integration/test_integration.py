9db9ce54c26b3f36500109454c11a43d2bc171cf
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-04-27 20:07:23 +0200

    Updates postgres version in itests

--- orc8r-orchestrator-operator/tests/integration/test_integration.py
+++ orc8r-orchestrator-operator/tests/integration/test_integration.py
@@ -51,7 +51,9 @@ class TestOrchestrator:
 
     @staticmethod
     async def _deploy_postgresql(ops_test):
-        await ops_test.model.deploy(DB_APPLICATION_NAME, application_name=DB_APPLICATION_NAME)
+        await ops_test.model.deploy(
+            DB_APPLICATION_NAME, application_name=DB_APPLICATION_NAME, channel="14/stable"
+        )
 
     @staticmethod
     async def _deploy_tls_certificates_operator(ops_test):
