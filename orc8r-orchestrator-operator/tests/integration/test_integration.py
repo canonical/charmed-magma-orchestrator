9643395934d90fa2e4792c67dbfb560406930085
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-05-03 15:17:03 +0000

    Adds trust to postgres

--- orc8r-orchestrator-operator/tests/integration/test_integration.py
+++ orc8r-orchestrator-operator/tests/integration/test_integration.py
@@ -52,7 +52,10 @@ class TestOrchestrator:
     @staticmethod
     async def _deploy_postgresql(ops_test):
         await ops_test.model.deploy(
-            DB_APPLICATION_NAME, application_name=DB_APPLICATION_NAME, channel="14/stable"
+            DB_APPLICATION_NAME,
+            application_name=DB_APPLICATION_NAME,
+            channel="14/stable",
+            trust=True,
         )
 
     @staticmethod
